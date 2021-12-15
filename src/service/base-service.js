062b8de5ddff980c8ac38e5efb7769d2cae74898
TonikX <antongovorov@gmail.com>
2021-12-15 10:23:52 +0300

    fix update profile data

--- src/service/base-service.js
+++ src/service/base-service.js
@@ -25,6 +25,12 @@ export default class BaseService {
         });
     }
 
+    patch(url, patchData) {
+        return new Promise((successFn, errorFn) => {
+            this.getAxios().patch(url, patchData).then(successFn).catch(errorFn);
+        });
+    }
+
     interceptSuccessResponse = (response) => {
         return {
             ...response,
