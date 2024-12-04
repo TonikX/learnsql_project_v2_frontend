bf5d6bee139ce040a025385a4171d1f8262d81ab
TonikX <antongovorov@gmail.com>
2024-12-05 01:54:38 +0300

    [ref] Откат ненужных коммитов

--- src/store/logics.js
+++ src/store/logics.js
@@ -6,17 +6,6 @@ import CourseLogic from '../containers/Course/logics';
 import ResetPasswordLogic from '../containers/ResetPassword/logics';
 import MainLogic from '../layout/logics';
 import FeedbackLogic from '../containers/Feedback/logics';
-import DatabasesLogic from '../containers/Databases/logics';
-import DatabaseLogic from '../containers/Database/logics';
-import CreateDatabaseLogic from '../containers/CreateDatabase/logics';
-import ThemesLogic from '../containers/Themes/logics';
-import CoursesList from '../containers/CoursesList/logics';
-import CreateCourse from '../containers/CreateCourse/logics';
-import Tasks from '../containers/Tasks/logics';
-import CreateTask from '../containers/CreateTask/logics';
-import CreateTopic from '../containers/CreateTopic/logics';
-import Topics from '../containers/Topics/logics';
-import Sections from '../containers/Sections/logics';
 import StatisticLogic from "../containers/Statistic/logics";
 
 export default [
@@ -28,16 +17,5 @@ export default [
     ...CourseLogic,
     ...ResetPasswordLogic,
     ...FeedbackLogic,
-    ...DatabasesLogic,
-    ...DatabaseLogic,
-    ...CreateDatabaseLogic,
-    ...ThemesLogic,
-    ...CoursesList,
-    ...CreateCourse,
-    ...Tasks,
-    ...CreateTask,
-    ...CreateTopic,
-    ...Topics,
-    ...Sections,
    ...StatisticLogic
 ];
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,3 +1,4 @@
+
 import {combineReducers} from "redux";
 import {GENERAL_PATH as signInPath, reducer as signInReducer} from "../containers/SignIn/reducer";
 import {GENERAL_PATH as signUpPath, reducer as signUpReducer} from "../containers/SignUp/reducer";
@@ -7,17 +8,6 @@ import {GENERAL_PATH as homePath, reducer as homeReducer} from "../containers/Co
 import {GENERAL_PATH as resetPasswordPath, reducer as resetPasswordReducer} from "../containers/ResetPassword/reducer";
 import {GENERAL_PATH as mainPath, reducer as mainReducer} from "../layout/reducer";
 import {GENERAL_PATH as feedbackPath, reducer as feedbackReducer} from "../containers/Feedback/reducer";
-import {GENERAL_PATH as databasesPath, reducer as databasesReducer} from "../containers/Databases/reducer";
-import {GENERAL_PATH as databasePath, reducer as databaseReducer} from "../containers/Database/reducer";
-import {GENERAL_PATH as createDatabasePath, reducer as createDatabaseReducer} from "../containers/CreateDatabase/reducer";
-import {GENERAL_PATH as themesPath, reducer as themesReducer} from "../containers/Themes/reducer";
-import {GENERAL_PATH as coursesListPath, reducer as coursesListReducer} from "../containers/CoursesList/reducer";
-import {GENERAL_PATH as createCoursePath, reducer as createCourseReducer} from "../containers/CreateCourse/reducer";
-import {GENERAL_PATH as tasksPath, reducer as tasksReducer} from "../containers/Tasks/reducer";
-import {GENERAL_PATH as createTaskPath, reducer as createTaskReducer} from "../containers/CreateTask/reducer";
-import {GENERAL_PATH as createTopicPath, reducer as createTopicReducer} from "../containers/CreateTopic/reducer";
-import {GENERAL_PATH as topicsPath, reducer as topicsReducer} from "../containers/Topics/reducer";
-import {GENERAL_PATH as sectionsPath, reducer as sectionsReducer} from "../containers/Sections/reducer";
 import {GENERAL_PATH as statisticPath, reducer as statisticReducer} from "../containers/Statistic/reducer";
 
 export default combineReducers({
@@ -29,16 +19,6 @@ export default combineReducers({
     [homePath]: homeReducer,
     [resetPasswordPath]: resetPasswordReducer,
     [feedbackPath] : feedbackReducer,
-    [databasesPath] : databasesReducer,
-    [databasePath] : databaseReducer,
-    [createDatabasePath] : createDatabaseReducer,
-    [themesPath] : themesReducer,
-    [coursesListPath] : coursesListReducer,
-    [createCoursePath] : createCourseReducer,
-    [tasksPath] : tasksReducer,
-    [createTaskPath]: createTaskReducer,
-    [createTopicPath]: createTopicReducer,
-    [topicsPath]: topicsReducer,
-    [sectionsPath]: sectionsReducer,
     [statisticPath] : statisticReducer,
+
 });
